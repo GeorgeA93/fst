da479d563e8f7d5e18164c46f97839eb3090b8b1
GeorgeA93 <george.allen93@outlook.com>
2017-02-16 20:14:11 +0000

    added eslint, closes #1

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ import controllers from './controllers';
 
 const app = express();
 
-app.use(express.static(__dirname + '/public')); // setup the public (assets) directory
+app.use(express.static(`${__dirname}/public`)); // setup the public (assets) directory
 
 // setup the rendering engine
-app.set('views', __dirname + '/views');
-app.set('view engine', 'pug')
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'pug');
 
 // attach our middleware
 app.use(sampleMiddleware);
@@ -22,8 +22,8 @@ app.get('/', (req, res) => {
 });
 
 // start listening on port 3000
-const server = app.listen(3000, function () {
-    console.log('Listening on port 3000...')
+const server = app.listen(3000, () => {
+    console.log('Listening on port 3000...');
 });
 
 export default server;
